d7b8bb7224bcea21f696eae4a1b891d7c5a60a14
Martijn Luinstra <martijnluinstra@gmail.com>
2014-08-21 00:50:10 +0200

    Update models
    Add more user information, purchases and products

--- streep/models.py
+++ streep/models.py
@@ -1,13 +1,48 @@
 from streep import db
+from datetime import datetime
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
-    credits = db.Column(db.Integer)
+    name = db.Column(db.String(255), nullable=False)
+    address = db.Column(db.String(255), nullable=False)
+    place_of_residence = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=False)
+    iban = db.Column(db.String(34), nullable=False)
+    date_of_birth = db.Column(db.DateTime(), nullable=True)
+    purchases = db.relationship('Purchase', backref='user',
+                                lazy='dynamic')
 
-    def __init__(self, name, credits=0):
+    def __init__(self, name, address, place_of_residence, email, iban, date_of_birth=None):
         self.name = name
-        self.credits = credits
+        self.address = address
+        self.place_of_residence = place_of_residence
+        self.email = email
+        self.iban = iban
+        self.date_of_birth = date_of_birth
 
-    def consume(self, amount):
-        self.credits += amount
+
+class Purchase(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    timestamp = db.Column(db.DateTime(), nullable=False)
+    undone = db.Column(db.Boolean())
+
+    def __init__(self, user_id, product_id):
+        self.user_id = user_id
+        self.product_id = product_id
+        self.timestamp = datetime.utcnow()
+        self.undone = True
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Integer(), nullable=False)
+    age_limit = db.Column(db.Boolean())
+
+    def __init__(self, name, price=1, age_limit=False):
+        self.name = name
+        self.price = price
+        self.age_limit = false
