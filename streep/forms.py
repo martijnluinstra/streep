f61c04692be8aaf209aa114feeb06fb79e1937ef
Martijn Luinstra <martijnluinstra@gmail.com>
2014-08-22 03:36:22 +0200

    Use the right validators

--- streep/forms.py
+++ streep/forms.py
@@ -2,15 +2,16 @@ from flask.ext.wtf import Form
 from wtforms import TextField, BooleanField, IntegerField, DateTimeField, validators
 
 
-class AddUserForm(Form):
-    name = TextField('Name', [validators.Required(message='Name is required')])
-    address = TextField('Address', [validators.Required(message='Address is required')])
-    city = TextField('Place of residence', [validators.Required(message='Place of residence is required')])
-    email = TextField('Email address', [validators.Required(message='Email is required'), validators.Email(message='Invalid email address')])
+class UserForm(Form):
+    name = TextField('Name', [validators.InputRequired(message='Name is required')])
+    address = TextField('Address', [validators.InputRequired(message='Address is required')])
+    city = TextField('Place of residence', [validators.InputRequired(message='Place of residence is required')])
+    email = TextField('Email address', [validators.InputRequired(message='Email is required'), validators.Email(message='Invalid email address')])
     iban = TextField('IBAN')
-    birthday  = DateTimeField('Birthday', format='%d-%m-%Y')
+    birthday  = DateTimeField('Birthday', format='%d-%m-%Y', validators=[validators.Optional(strip_whitespace=True)])
 
-class AddProductForm(Form):
-    name = TextField('Name', [validators.Required(message='Name is required')])
-    price = IntegerField('Price (credits)', [validators.Required(message='Price is required')])
+
+class ProductForm(Form):
+    name = TextField('Name', [validators.InputRequired(message='Name is required')])
+    price = IntegerField('Price (credits)', [validators.InputRequired(message='Price is required')])
     age_limit = BooleanField('Age limit')
