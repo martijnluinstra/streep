92f0cb3d3902a7212d087c81dbfe0fae91884834
Martijn Luinstra <martijnluinstra@gmail.com>
2014-08-31 22:35:32 +0200

    Create products page

--- streep/frontend.py
+++ streep/frontend.py
@@ -17,12 +17,13 @@ def utility_processor():
     return dict(format_price=format_price, is_eligible=is_eligible)
 
 @app.route('/', methods=['GET'])
+@app.route('/users', methods=['GET'])
 def view_home():
     """ View all users, ordered by name """
     spend_subq = db.session.query(Purchase.user_id.label("user_id"), db.func.sum(Product.price).label("spend")).join(Product, Purchase.product_id==Product.id).filter(Purchase.undone == False).group_by(Purchase.user_id).subquery()
-    users =  db.session.query(User, spend_subq.c.spend).outerjoin(spend_subq, spend_subq.c.user_id==User.id).all()
+    users =  db.session.query(User, spend_subq.c.spend).outerjoin(spend_subq, spend_subq.c.user_id==User.id).order_by(User.name).all()
     # users = User.query.order_by(User.name).all()
-    products = Product.query.all()
+    products = Product.query.order_by(Product.priority.desc()).all()
     return render_template('index.html', users=users, products=products)
 
 
@@ -56,45 +57,12 @@ def edit_user(user_id):
     return render_template('user_add.html', form=form, mode='edit', id=user.id)
 
 
-@app.route('/products/add', methods=['GET', 'POST'])
-def add_product():
-    """ 
-    Try to create a new product.
-    """
-    form = ProductForm()
-    if form.validate_on_submit():
-        product = Product(form.name.data, form.price.data, form.priority.data, form.age_limit.data)
-        db.session.add(product)
-        db.session.commit()
-        return redirect(url_for('view_home'))
-    return render_template('product_add.html', form=form, mode='add')
-
-
-@app.route('/products/<int:product_id>', methods=['GET', 'POST'])
-def edit_product(product_id):
-    product = Product.query.filter_by(id = product_id).first_or_404()
-    form = ProductForm(request.form, product)
-    if form.validate_on_submit():
-        form.populate_obj(product)
-        db.session.commit()
-        return redirect(url_for('view_home'))
-    return render_template('product_add.html', form=form, mode='edit', id=product.id)
-
-
 @app.route('/users/<int:user_id>/history', methods=['GET'])
 def history(user_id):
     # purchases = Purchase.query.filter_by(user_id = user_id).all()
     purchases = db.session.query(Purchase, Product.name, Product.price).join(Product, Purchase.product_id==Product.id).filter(Purchase.user_id == user_id).order_by(Purchase.id.desc()).all()
     user = User.query.filter_by(id = user_id).first_or_404()
     return render_template('user_history.html', purchases=purchases, user=user)
-
-
-@app.route('/purchase/<int:purchase_id>/undo', methods=['GET'])
-def undo(purchase_id):
-    purchase = Purchase.query.filter_by(id = purchase_id).first_or_404()
-    purchase.undone = True
-    db.session.commit()
-    return redirect(url_for('history', user_id = purchase.user_id))
 
 
 @app.route('/purchase', methods=['POST'])
@@ -115,3 +83,45 @@ def batch_undo():
         purchase.undone = True
     db.session.commit()
     return 'Purchases undone', 201
+
+
+@app.route('/purchase/<int:purchase_id>/undo', methods=['GET'])
+def undo(purchase_id):
+    purchase = Purchase.query.filter_by(id = purchase_id).first_or_404()
+    purchase.undone = True
+    db.session.commit()
+    return redirect(url_for('history', user_id = purchase.user_id))
+
+
+@app.route('/products', methods=['GET', 'POST'])
+def view_products():
+    """ 
+    View all products.
+    """
+    products = Product.query.order_by(Product.priority.desc()).all()
+    return render_template('products.html', products=products)
+
+
+@app.route('/products/add', methods=['GET', 'POST'])
+def add_product():
+    """ 
+    Try to create a new product.
+    """
+    form = ProductForm()
+    if form.validate_on_submit():
+        product = Product(form.name.data, form.price.data, form.priority.data, form.age_limit.data)
+        db.session.add(product)
+        db.session.commit()
+        return redirect(url_for('view_products'))
+    return render_template('product_add.html', form=form, mode='add')
+
+
+@app.route('/products/<int:product_id>', methods=['GET', 'POST'])
+def edit_product(product_id):
+    product = Product.query.filter_by(id = product_id).first_or_404()
+    form = ProductForm(request.form, product)
+    if form.validate_on_submit():
+        form.populate_obj(product)
+        db.session.commit()
+        return redirect(url_for('view_products'))
+    return render_template('product_add.html', form=form, mode='edit', id=product.id)
