0a5a7bfd3cc0f0a4140a8ef6e686f05176a803ce
Martijn Luinstra <martijnluinstra@gmail.com>
2015-06-08 21:14:10 +0200

    Delete config.py (as we have a default now)

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-SECRET_KEY = '-I\xc1me\x91\xd5\xfcQ\xd2V:\xc2W\xba3\xb18d\x08\xa4\x80\xe6E'
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
